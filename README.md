adf260409228ae7de2902003335dcf6cba551d08
Anderson Ferreira <andersonvaf@gmail.com>
2019-05-25 14:25:32 -0300

    First commit. First working version with Fuzzy C-Means algorithm.

---
+++ README.md
@@ -0,0 +1,6 @@
+sklearn-fuzzy
+============
+
+`sklearn_fuzzy` is a fork from `scikit-fuzzy` package with sklearn-ish methods.
+
+Original scikit-fuzzy code and docs can be found at: https://github.com/scikit-fuzzy/scikit-fuzzy [![DOI](https://zenodo.org/badge/8872608.svg)](https://zenodo.org/badge/latestdoi/8872608)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='sklearn_fuzzy',
+     version='0.0.1',
+     author="Anderson Vinicius",
+     author_email="andersonvaf@gmail.com",
+     description="`sklearn_fuzzy` is a fork from `scikit-fuzzy` package with sklearn-ish methods.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/andersonvaf/sklearn_fuzzy",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+)
